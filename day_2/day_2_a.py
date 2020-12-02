5dcf55e4e2fd92fe5d4f7e3db738546bdd6d7457
FilippoUslenghi <us98@hotmail.it>
2020-12-02 14:48:51 +0100

    day 2

--- day_2/day_2_a.py
+++ day_2/day_2_a.py
@@ -1,6 +1,9 @@
 l = []
 
-with open("inpt.txt") as f:
-    for line in f:
-        l.append(line)
-    
+# with open("inpt.txt") as f:
+#     for line in f:
+#         l.append(line)
+
+with open("input.txt") as f:
+    line = f.read()
+    print(line)
