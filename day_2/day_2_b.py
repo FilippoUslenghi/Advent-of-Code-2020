dcd95fe10d3a62fd343a6421375ecf5e82176dc2
FilippoUslenghi <us98@hotmail.it>
2020-12-02 17:28:29 +0100

    day 2 b

---
+++ day_2/day_2_b.py
@@ -0,0 +1,15 @@
+with open("input.txt") as f:
+
+    validPwds = 0
+    for line in f:
+        policy = line.split(":")[0]
+        pwd = line.split(":")[1][1:-1]
+        limits = policy.split(" ")[0]
+        letter = policy.split(" ")[1]
+        a = int(limits.split("-")[0])
+        b = int(limits.split("-")[1])
+
+        if letter == pwd[a-1] and letter != pwd[b-1] or letter != pwd[a-1] and letter == pwd[b-1]:
+            validPwds += 1
+
+    print(validPwds)
