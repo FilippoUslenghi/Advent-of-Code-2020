ac3366fd6f76c6f6a2dc92b26411e5550bea8531
FilippoUslenghi <us98@hotmail.it>
2020-12-02 16:19:25 +0100

    day_2

--- day_2/day_2_a.py
+++ day_2/day_2_a.py
@@ -1,9 +1,27 @@
-l = []
-
-# with open("inpt.txt") as f:
-#     for line in f:
-#         l.append(line)
-
 with open("input.txt") as f:
-    line = f.read()
-    print(line)
+
+    validPwds = 0
+    iterations = 1
+    for line in f:
+        policy = line.split(":")[0]
+        pwd = line.split(":")[1][1:-1]
+        limits = policy.split(" ")[0]
+        letter = policy.split(" ")[1]
+        lower_limit = int(min(limits.split("-")))
+        upper_limit = int(max(limits.split("-")))
+
+        count = 0
+        for char in pwd:
+            if char == letter:
+                count += 1
+
+        if count >= lower_limit and count <= upper_limit:
+            validPwds += 1
+
+        # if iterations == 8:
+        #     print(line[:-1])
+        #     break
+
+        # iterations += 1
+
+    print(validPwds)
