be0c8e7db825a99c277242371cba18c5055e912f
FilippoUslenghi <us98@hotmail.it>
2020-12-03 17:35:27 +0100

    day 3

--- day_3/day_3_b.py
+++ day_3/day_3_b.py
@@ -23,17 +23,12 @@ def lastSlope():
 
 with open("input.txt") as f:
     first = firstSlopes(1)
-    print("first:", first)
     f.seek(0, 0)
     second = firstSlopes(3)
-    print("second:", second)
     f.seek(0, 0)
     third = firstSlopes(5)
-    print("third:", third)
     f.seek(0, 0)
     fourth = firstSlopes(7)
-    print("fourth:", fourth)
     f.seek(0, 0)
     fifth = lastSlope()
-    print("fifth:", fifth)
     print("result:", first*second*third*fourth*fifth)
