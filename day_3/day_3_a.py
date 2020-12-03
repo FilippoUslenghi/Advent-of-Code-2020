25c310217259aa4f5e421a5a478df4ce3d84a298
FilippoUslenghi <us98@hotmail.it>
2020-12-03 16:03:40 +0100

    day 3

--- day_3/day_3_a.py
+++ day_3/day_3_a.py
@@ -1,4 +1,10 @@
 with open("input.txt") as f:
-    for line in f:
-        print(line)
 
+    i = 0
+    count = 0
+    for line in f:
+        line = line[:-1]
+        i = (i + 3) % len(line)
+        if line[i] == "#":
+            count += 1
+    print(count)
