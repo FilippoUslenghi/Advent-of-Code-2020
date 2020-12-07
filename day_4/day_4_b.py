71884bc2e382bfeed16537b429a5b33b4eefc5cd
FilippoUslenghi <us98@hotmail.it>
2020-12-07 20:16:34 +0100

    day 4

--- day_4/day_4_b.py
+++ day_4/day_4_b.py
@@ -5,13 +5,14 @@ with open("input.txt", "a") as myfile:
 
 with open("input.txt") as f:
     passport = ""
-    patterns = ["byr:(19[2-9]\\d( |\n)|200[0-2]( |\n))", "iyr:201\\d( |\n)|2020( |\n)", "eyr:202\\d( |\n)|2030( |\n)", "hgt:((1[5-8]\\d|19[0-3])cm( |\n)|(59|6\\d|7[0-6])in( |\n))", "hcl:#(\\d|[a-f]){6}( |\n)", "ecl:(amb|blu|brn|gry|grn|hzl|oth){1}( |\n)", "pid:\\d{9}( |\n)"]
+    patterns = ["byr:(19[2-9]\\d|200[0-2])", "iyr:201\\d|2020", "eyr:202\\d|2030", "hgt:((1[5-8]\\d|19[0-3])cm|(59|6\\d|7[0-6])in)", "hcl:#(\\d|[a-f]){6}", "ecl:(amb|blu|brn|gry|grn|hzl|oth){1}", "pid:\\d{9}"]
     count = 0
     for line in f:
         passport += line
         if line == "\n":
             matches = []
             for pattern in patterns:
+                pattern += "( |\n)"
                 matches.append(re.search(pattern, passport))
             if all(matches):
                 count += 1
