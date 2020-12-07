3d60b226e6c53c0f93ad915251ec94eb8971d928
FilippoUslenghi <us98@hotmail.it>
2020-12-07 02:37:10 +0100

    day 6 a

---
+++ day_6/day_6_a.py
@@ -0,0 +1,19 @@
+with open("input.txt", "a") as myfile:
+    myfile.write("\n")
+
+with open("input.txt") as f:
+
+    sum = 0
+    groupAnswer = set()
+    s = set()
+    iterations = 1
+    for line in f:
+        line = line[:-1]
+        s = set(line)
+        groupAnswer = groupAnswer.union(s)
+
+        if len(line) == 0:
+            sum += len(groupAnswer)
+            groupAnswer = set()
+
+    print(sum)
